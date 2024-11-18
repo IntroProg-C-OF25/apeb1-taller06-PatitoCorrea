7e08b58f6e758c6d0bbab13734ee733b59201ddd
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-11-17 20:13:16 -0500

    resolucion del problema 1

--- ProyectoTaller6/src/Problema1_Facturacion2Productos.java
+++ ProyectoTaller6/src/Problema1_Facturacion2Productos.java
@@ -28,3 +28,4 @@ public class Problema1_Facturacion2Productos {
     }
 
 }
+
--- ProyectoTaller6/src/Problema5_TipoOperacion.java
+++ ProyectoTaller6/src/Problema5_TipoOperacion.java
@@ -1,12 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author patito
- */
+import java.util.Scanner;
+
 public class Problema5_TipoOperacion {
+    public static void main(String[] args) {
+        int numOperacion;
+        String tipoOperacion;
+        Scanner tcl = new Scanner(System.in);
+        System.out.print("Ingrese un número del 1 al 4 que represente una operación matemática:");
+        System.out.print("1: Suma");
+        System.out.print("2: Resta");
+        System.out.print("3: Multiplicación");
+        System.out.print("4: División");
+        numOperacion = tcl.nextInt();
+        switch (numOperacion) {
+            case 1:
+                tipoOperacion = "Suma";
+                break;
+            case 2:
+                tipoOperacion = "Resta";
+                break;
+            case 3:
+                tipoOperacion = "Multiplicación";
+                break;
+            case 4:
+                tipoOperacion = "División";
+                break;
+            default:
+                tipoOperacion = "Operación no válida";
+                break;
+        }
+        System.out.println("La operación seleccionada es: " + tipoOperacion);
+    }
     
 }
